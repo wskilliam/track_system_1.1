9d48d823043992a457da9dd9386d2030c227ebe1
William R. <55420017+wskilliam@users.noreply.github.com>
2024-08-09 06:47:54 -0300

    Segundo Commit 1.1
    Sistema desenvolvido em Laravel 9.x com Inertia.js e PHP 7.1 ou superior. O FRONT-END utilizando o Vue Composition API e o banco de dados será MySQL.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+    ],
+    
+});
